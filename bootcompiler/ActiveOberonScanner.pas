0d0412d14899ab6bb6f0414deb591dd0acb38f98
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-08-04 16:04:28 +0200

    Updated scanner

--- bootcompiler/ActiveOberonScanner.pas
+++ bootcompiler/ActiveOberonScanner.pas
@@ -10,28 +10,28 @@ const
 
     (* Symbols used between scanner and parser *)
     Symb_None = 0;
-    Symb_Equal = 1; Symb_DotEqual = 2; Symb_Unequal = 3; Symb_DotUnequal = 4; Symb_Less = 5; Symb_DotLess = 6; 
-	Symb_LessEqual = 7; Symb_DotLessEqual = 8; Symb_Greater = 9; Symb_DotGreater = 10; Symb_GreaterEqual = 11; Symb_DotGreaterEqual = 12; 
-	Symb_LessLessQ = 13; Symb_GreaterGreaterQ = 14; Symb_Questionmarks = 15; Symb_ExclamationMarks = 16; Symb_In = 17; Symb_Is = 18; 
-    Symb_Times = 19; Symb_TimesTimes = 20; Symb_DotTimes = 21; Symb_PlusTimes = 22; Symb_Slash = 23; Symb_Backslash = 24; 
-	Symb_DotSlash = 25; Symb_Div = 26; Symb_Mod = 27; Symb_And = 28; 
-    Symb_Or = 29; Symb_Plus = 30; Symb_Minus = 31; 
+    Symb_Equal = 1; Symb_DotEqual = 2; Symb_Unequal = 3; Symb_DotUnequal = 4; Symb_Less = 5; Symb_DotLess = 6;
+    Symb_LessEqual = 7; Symb_DotLessEqual = 8; Symb_Greater = 9; Symb_DotGreater = 10; Symb_GreaterEqual = 11; Symb_DotGreaterEqual = 12;
+    Symb_LessLessQ = 13; Symb_GreaterGreaterQ = 14; Symb_Questionmarks = 15; Symb_ExclamationMarks = 16; Symb_In = 17; Symb_Is = 18;
+    Symb_Times = 19; Symb_TimesTimes = 20; Symb_DotTimes = 21; Symb_PlusTimes = 22; Symb_Slash = 23; Symb_Backslash = 24;
+    Symb_DotSlash = 25; Symb_Div = 26; Symb_Mod = 27; Symb_And = 28;
+    Symb_Or = 29; Symb_Plus = 30; Symb_Minus = 31;
     Symb_Not = 32;
-    Symb_LeftParenthesis = 33; Symb_LeftBracket = 34; Symb_LeftBrace = 35; Symb_Number = 36; Symb_Character = 37; Symb_String = 38; 
-	Symb_Nil = 39; Symb_Imag = 40; Symb_True = 41; Symb_False = 42; Symb_Self = 43; Symb_Result = 44; 
-	Symb_Identifier = 45; 
-    Symb_If = 46; Symb_Case = 47; Symb_While = 48; Symb_Repeat = 49; Symb_For = 50; Symb_Loop = 51; 
-	Symb_With = 52; Symb_Exit = 53; Symb_Await = 54; Symb_Return = 55; Symb_Begin = 56; 
-    Semicolon = 57; Transpose = 58; RightBrace = 59; RightBracket = 60; RightParenthesis = 61; Questionmark = 62; 
-	ExclamationMark = 63; LessLess = 64; GreaterGreater = 65; Upto = 66; Arrow = 67; Period = 68; 
-	Symb_Comma = 69; Symb_Colon = 70; Symb_Of = 71; Symb_Then = 72; Symb_Do = 73; Symb_To = 74; 
-	Symb_By = 75; Symb_Becomes = 76; Symb_Bar = 77; Symb_End = 78; Symb_Else = 79; Symb_Elsif = 80; 
-	Symb_Until = 81; Symb_Finally = 82; 
-    Symb_Code = 83; Symb_Const = 84; Symb_Type = 85; Symb_Var = 86; Symb_Out = 87; Symb_Procedure = 88; 
-	Symb_Operator = 89; Symb_Import = 90; Symb_Definition = 91; Symb_Module = 92; Symb_Cell = 93; Symb_CellNet = 94; 
-	Symb_Extern = 95;
-    Symb_Array = 96; Symb_Object = 97; Symb_Record = 98; Symb_Pointer = 99; Symb_Enum = 100; Symb_Port = 101; 
-	Symb_Address = 102; Symb_Size = 103; Symb_Alias = 104; 
+    Symb_LeftParenthesis = 33; Symb_LeftBracket = 34; Symb_LeftBrace = 35; Symb_Number = 36; Symb_Character = 37; Symb_String = 38;
+    Symb_Nil = 39; Symb_Imag = 40; Symb_True = 41; Symb_False = 42; Symb_Self = 43; Symb_Result = 44;
+    Symb_Identifier = 45;
+    Symb_If = 46; Symb_Case = 47; Symb_While = 48; Symb_Repeat = 49; Symb_For = 50; Symb_Loop = 51;
+    Symb_With = 52; Symb_Exit = 53; Symb_Await = 54; Symb_Return = 55; Symb_Begin = 56;
+    Symb_Semicolon = 57; Symb_Transpose = 58; Symb_RightBrace = 59; Symb_RightBracket = 60; Symb_RightParenthesis = 61; Symb_Questionmark = 62;
+    Symb_ExclamationMark = 63; Symb_LessLess = 64; Symb_GreaterGreater = 65; Symb_Upto = 66; Symb_Arrow = 67; Symb_Period = 68;
+    Symb_Comma = 69; Symb_Colon = 70; Symb_Of = 71; Symb_Then = 72; Symb_Do = 73; Symb_To = 74;
+    Symb_By = 75; Symb_Becomes = 76; Symb_Bar = 77; Symb_End = 78; Symb_Else = 79; Symb_Elsif = 80;
+    Symb_Until = 81; Symb_Finally = 82;
+    Symb_Code = 83; Symb_Const = 84; Symb_Type = 85; Symb_Var = 86; Symb_Out = 87; Symb_Procedure = 88;
+    Symb_Operator = 89; Symb_Import = 90; Symb_Definition = 91; Symb_Module = 92; Symb_Cell = 93; Symb_CellNet = 94;
+    Symb_Extern = 95;
+    Symb_Array = 96; Symb_Object = 97; Symb_Record = 98; Symb_Pointer = 99; Symb_Enum = 100; Symb_Port = 101;
+    Symb_Address = 102; Symb_Size = 103; Symb_Alias = 104;
 
 type
 
